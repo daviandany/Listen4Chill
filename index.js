f9a08e116b872965a86b3d9e6988250504278138
davi-y08 <oisyzenx@gmail.com>
2026-01-11 18:15:15 -0300

    play audio

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const musicas = [
-    {id: 1, nome: "", img: ".src", audio: ""},
-    {id: 2, nome: "", img: ".src", audio: ""},
-    {id: 3, nome: "", img: ".src", audio: ""},
-    {id: 4, nome: "", img: ".src", audio: ""},
+    {id: 1, nome: "elton", img: ".src", audio: ""},
+    {id: 2, nome: "elton", img: ".src", audio: ""},
+    {id: 3, nome: "elton", img: ".src", audio: ""},
+    {id: 4, nome: "elton", img: ".src", audio: ""},
 ];
 
 const container = document.getElementById("lista-musicas");
@@ -11,11 +11,17 @@ musicas.forEach(musica => {
     const div = document.createElement('div')
     div.classList.add('musica')
 
+    const audio = document.createElement('audio')
+    audio.src = musica.audio
+
     div.innerHTML = `
         <img src="${musica.img}" alt="${musica.nome}">
         <p>${musica.nome}</p>
-        <audio controls src="${musica.audio}"></audio>
     `;
+
+    div.addEventListener('click', () => {
+       audio.play() 
+    })
 
     container.appendChild(div);
 })
