b96842e2f0f458afd2ce3d1a8f4809cc14a68131
davi-y08 <oisyzenx@gmail.com>
2026-01-11 18:47:18 -0300

    tesyh gay

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
 const musicas = [
-    {id: 1, nome: "elton", img: ".src", audio: ""},
-    {id: 2, nome: "elton", img: ".src", audio: ""},
-    {id: 3, nome: "elton", img: ".src", audio: ""},
-    {id: 4, nome: "elton", img: ".src", audio: ""},
+    {id: 1, nome: "elton", img: "./arthur.png", audio: "./lofi.mp3", key: "w"},
+    {id: 2, nome: "elton", img: "./arthur.png", audio: "./som2.mp3", key: "a"},
+    {id: 3, nome: "elton", img: "./arthur.png", audio: "./som3.mp3", key: "s"},
+    {id: 4, nome: "elton", img: "./arthur.png", audio: "./som4.mp3", key: "d"},
 ];
 
 const container = document.getElementById("lista-musicas");
 
 musicas.forEach(musica => {
-    const div = document.createElement('div')
-    div.classList.add('musica')
+    const div = document.createElement('div');
+    div.classList.add('musica');
 
-    const audio = document.createElement('audio')
-    audio.src = musica.audio
+    const audio = document.createElement('audio');
+    audio.src = musica.audio;
 
     div.innerHTML = `
         <img src="${musica.img}" alt="${musica.nome}">
-        <p>${musica.nome}</p>
+        <p>${musica.nome} (${musica.key})</p>
     `;
 
+    div.appendChild(audio);
+
+    // Clique
     div.addEventListener('click', () => {
-       audio.play() 
-    })
+        audio.currentTime = 0;
+        audio.play();
+    });
+
+    document.addEventListener('keydown',
+        function(event){
+            if (event.key === musica.key) {
+                audio.currentTime = 0;
+                audio.play();
+            }
+        }
+    );
 
     container.appendChild(div);
-})
+});
