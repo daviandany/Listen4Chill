fd2eb844eb02e3998b3c71f41407dcd4af236997
davi-y08 <oisyzenx@gmail.com>
2026-01-11 17:48:21 -0300

    ..

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-function teste(){
-    console.log('teste')
-}
+const musicas = [
+    {id: 1, nome: "", img: ".src", audio: ""},
+    {id: 2, nome: "", img: ".src", audio: ""},
+    {id: 3, nome: "", img: ".src", audio: ""},
+    {id: 4, nome: "", img: ".src", audio: ""},
+];
+
+const container = document.getElementById("lista-musicas");
+
+musicas.forEach(musica => {
+    const div = document.createElement('div')
+    div.classList.add('musica')
+
+    div.innerHTML = `
+        <img src="${musica.img}" alt="${musica.nome}">
+        <p>${musica.nome}</p>
+        <audio controls src="${musica.audio}"></audio>
+    `;
+
+    container.appendChild(div);
+})
