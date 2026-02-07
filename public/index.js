708a67cda534e2da4dd0d93efb84543f253ef71d
daviandany <andanydavi2@gmail.com>
2026-02-07 16:38:47 -0300

    fix: opacity do musics when random

--- public/index.js
+++ public/index.js
@@ -1,9 +1,9 @@
 //array de objetos
-const musicas =[
-    { id: 1, nome: "Lofi Relax", img: "img/exemplo1.jpg", audio: "musicas/musica1.mp3",key:"1" },
-    { id: 2, nome: "Estudo Intenso", img: "img/exemplo1.jpg", audio: "musicas/musica2.mp3",key:"2"},
-    { id: 3, nome: "Noite de Chuva", img: "img/exemplo1.jpg", audio: "musicas/musica3.mp3",key:"3"},
-    { id: 4, nome: "Café da Manhã", img: "img/exemplo1.jpg", audio: "musicas/musica4.mp3",key:"4" },
+const musicas = [
+    { id: 1, nome: "Lofi Relax", img: "img/exemplo1.jpg", audio: "musicas/musica1.mp3", key: "1" },
+    { id: 2, nome: "Estudo Intenso", img: "img/exemplo1.jpg", audio: "musicas/musica2.mp3", key: "2" },
+    { id: 3, nome: "Noite de Chuva", img: "img/exemplo1.jpg", audio: "musicas/musica3.mp3", key: "3" },
+    { id: 4, nome: "Café da Manhã", img: "img/exemplo1.jpg", audio: "musicas/musica4.mp3", key: "4" },
 ];
 
 const $container = $(".lista-musicas");
@@ -14,16 +14,17 @@ let musicsPlaying = []
 function pausar() {
     document.querySelectorAll("audio").forEach(musica => {
         musica.pause();
-        musica.currentTime = 0;})
+        musica.currentTime = 0;
+    })
 }
 
 
 musicas.forEach(musica => {
-    const $div = $("<div>").addClass('musica'); 
+    const $div = $("<div>").addClass('musica');
 
     const $audio = $("<audio>").addClass('audio');
     $audio.attr("src", musica.audio);
-    
+
 
     $div.html(`
         <img src="${musica.img}" alt="${musica.nome}">
@@ -31,7 +32,7 @@ musicas.forEach(musica => {
     `);
 
 
-    $div.append($audio);    
+    $div.append($audio);
 
 
     //Click play/pause
@@ -43,12 +44,12 @@ musicas.forEach(musica => {
         key: musica.key,
         audio: $pureAudio
     })
-    
 
 
-//function to click event
-    $div.on('click', ()=>{ 
-        if($pureAudio.paused){
+
+    //function to click event
+    $div.on('click', () => {
+        if ($pureAudio.paused) {
             $(".musica").removeClass('opacityToMusics');
             pausar()
             $pureAudio.play();
@@ -57,23 +58,23 @@ musicas.forEach(musica => {
             $pureAudio.pause();
             $pureAudio.currentTime = 0;
             $($div).removeClass('opacityToMusics');
-            
+
         }
     });
 
 
 
-//function to key click event 
+    //function to key click event 
     $(document).on('keydown',
-        function(event){
+        function (event) {
 
             if (event.key === musica.key) {
-                if($pureAudio.paused){
+                if ($pureAudio.paused) {
                     $(".musica").removeClass('opacityToMusics');
                     pausar()
                     $pureAudio.play();
                     $($div).addClass('opacityToMusics');
-                }else{
+                } else {
                     $pureAudio.pause();
                     $pureAudio.currentTime = 0;
                     $($div).removeClass('opacityToMusics');
@@ -83,8 +84,8 @@ musicas.forEach(musica => {
         }
     );
 
-    
-    
+
+
 
     $container.append($div);
 });//forEach
@@ -95,12 +96,12 @@ var storingTheH1Text = $('h1').text();
 
 //hover to h1
 $("h1").hover(
-    function(){
-     if($(this).hasClass('changeTitle')){
-        $(this).removeClass('changeTitle').text(storingTheH1Text);
-         } else{
-        $(this).addClass('changeTitle').text("Listen For Chill");
-       }
+    function () {
+        if ($(this).hasClass('changeTitle')) {
+            $(this).removeClass('changeTitle').text(storingTheH1Text);
+        } else {
+            $(this).addClass('changeTitle').text("Listen For Chill");
+        }
     }
 )
 
@@ -109,41 +110,43 @@ function playRandomMusic() {
     $(".musica").removeClass('opacityToMusics');
 
     const randomIndex = Math.floor(Math.random() * musicas.length);
-    let music = musicsPlaying[randomIndex];
+    const music = musicsPlaying[randomIndex];
+    const $musicCard = $(".musica").eq(randomIndex);
 
-    music.audio.currentTime = 0
-    music.audio.play()
-
-    $(".musica").removeClass('opacityToMusics');
+    if (music && music.audio) {
+        music.audio.currentTime = 0;
+        music.audio.play();
+        $musicCard.addClass('opacityToMusics');
+    }
 }
 
 var colorsHidden = $(".hiddenColor").hide();
-$("#seeBackgroundButtons").on("click", function(){
+$("#seeBackgroundButtons").on("click", function () {
     $(colorsHidden).slideToggle();
 })
 
-$('#colorRed').on("click", function(event){
-$("body").toggleClass('redColor');
+$('#colorRed').on("click", function (event) {
+    $("body").toggleClass('redColor');
 })
 
-$("#colorBlue").on("click", function(){
+$("#colorBlue").on("click", function () {
     $("body").toggleClass('blueColor');
 })
 
-$('#colorAqua').on("click", function(){
+$('#colorAqua').on("click", function () {
     $("body").toggleClass('aquaColor');
 })
 
-$("#colorYellow").on("click", function(){
+$("#colorYellow").on("click", function () {
     $("body").toggleClass('yellowColor');
 })
 
-function resetColors(){
-$("body").removeClass("redColor blueColor aquaColor yellowColor")
+function resetColors() {
+    $("body").removeClass("redColor blueColor aquaColor yellowColor")
 }
 
-$("#backColor").on("click", function(){
-resetColors();
+$("#backColor").on("click", function () {
+    resetColors();
 })
 
 
